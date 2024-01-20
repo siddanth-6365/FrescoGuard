35431481a62f9827e673dc96748eba0200108df7
siddanth-6365 <siddanth6365@gmail.com>
2024-01-21 01:10:16 +0530

    added redirect

--- frontend/src/components/homepage/Hero/Hero.jsx
+++ frontend/src/components/homepage/Hero/Hero.jsx
@@ -1,24 +1,32 @@
-import Image from 'next/image';
-import './hero.css'
-import hero_img from '../assets/hero_img.png'
-import logo from "../assets/frescoGuard_icon.png"
+import Image from "next/image";
+import "./hero.css";
+import hero_img from "../assets/hero_img.png";
+import logo from "../assets/frescoGuard_icon.png";
+import Link from "next/link";
 
 const Hero = () => {
-    return (
-        <>
-        <Image src={logo}  alt="" className='logo'/>
-            <div className="hero">
-                <img src={hero_img} alt="" className='img'/>
-                <div className="gradient-overlay"></div>
-                <div className="heading">
-                    <h1 className="first">Agriculture Matter</h1>
-                    <h1 className="second">Effective Storage</h1>
-                    <p className="desc">Fresco Guard employs data-driven technology, real-time updates, and user feedback to revolutionize food storage, minimizing spoilage and contributing to a significant reduction in food waste</p>
-                    <button>Register / Sign In</button>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <Image src={logo} alt="" className="logo" />
+      <div className="hero">
+        <img src={hero_img} alt="" className="img" />
+        <div className="gradient-overlay"></div>
+        <div className="heading">
+          <h1 className="first">Agriculture Matter</h1>
+          <h1 className="second">Effective Storage</h1>
+          <p className="desc">
+            Fresco Guard employs data-driven technology, real-time updates, and
+            user feedback to revolutionize food storage, minimizing spoilage and
+            contributing to a significant reduction in food waste
+          </p>
+          <Link href="/auth/login">
+            {" "}
+            <button>Register / Sign In</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
